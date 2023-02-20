edb10ba05f71f0f56a9c256e9e637a72e691a68c
Doh1024 <dohyeongkim001024@gmail.com>
2023-02-19 20:12:28 -0600

    tweaks

--- src/components/Card.js
+++ src/components/Card.js
@@ -11,7 +11,10 @@ const Card = (props) => {
           <div class="p-5">
             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-dark-cyan">{props.prog}</h5>
             <p class="mb-3 font-normal italic text-gray-700 dark:text-dark-cyan">{props.level}</p>
-            <ShowMore maxHeight={150}>
+            <ShowMore maxHeight={150}
+            style={{fontWeight:'500', fontSize:'15px', padding:'10px'}}
+            styleButton={{backgroundColor:'#B7AAE2', borderRadius:'10px', padding:'10px 10px'}}
+            >
               <p class="mb-3 font-normal text-gray-700 dark:text-dark-cyan">{props.info}</p>
               <p class="mb-3 font-normal text-gray-700 dark:text-dark-cyan">{props.info2}</p>
             </ShowMore>        
